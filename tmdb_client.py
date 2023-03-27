3203866091ac40cc888e4047ffe72fac79807317
Dominik Gabinski <gabinski.dominik@gmail.com>
2023-03-27 20:10:22 +0200

    Add working list_type

--- tmdb_client.py
+++ tmdb_client.py
@@ -2,13 +2,17 @@ from flask import Flask
 import requests
 API_TOKEN =  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MWJhMWI0MzFhNjY2ZjQ5Mzg4OGNhYzRkYTc4MDQ2MyIsInN1YiI6IjY0MWFmOWQxOTVjMGFmMDBmNzA3YzE0MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DXd5aHVZNydzEVc-wrA_XlWgl2FMZ1ZvdyMqYeXEeQY"
 
-def get_popular_movies():
-    endpoint = "https://api.themoviedb.org/3/movie/popular"
-    api_token = API_TOKEN
+def get_movies(how_many,list_type):
+    data = get_movies_list(list_type)
+    return data["results"][:how_many]
+
+def get_movies_list(list_type):
+    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
     headers = {
-        "Authorization": f"Bearer {api_token}"
+        "Authorization": f"Bearer {API_TOKEN}"
     }
     response = requests.get(endpoint, headers=headers)
+    response.raise_for_status()
     return response.json()
 
 def get_poster_url(poster_api_path, size="w342"):
