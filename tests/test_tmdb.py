885ca3e35b75a39efa3e318808768fe4b4033039
Dominik Gabinski <gabinski.dominik@gmail.com>
2023-04-21 14:38:22 +0200

    Add Zadanie 2 moduł 14

--- tests/test_tmdb.py
+++ tests/test_tmdb.py
@@ -1,7 +1,8 @@
-import sys, requests
+import sys, requests, pytest
 sys.path.append(r'C:\Kodilla\Kurs\Movies\movies_catalogue')
 import tmdb_client
 from unittest.mock import Mock
+from main import app
 
 
 API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MWJhMWI0MzFhNjY2ZjQ5Mzg4OGNhYzRkYTc4MDQ2MyIsInN1YiI6IjY0MWFmOWQxOTVjMGFmMDBmNzA3YzE0MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DXd5aHVZNydzEVc-wrA_XlWgl2FMZ1ZvdyMqYeXEeQY"
@@ -23,7 +24,7 @@ def test_get_single_movie():
    single_movie = tmdb_client.get_single_movie(movie_id="238")
    assert single_movie["title"] == 'The Godfather'
 
-def test_get_movies_list(monkeypatch):
+def test_get_movies_list_1(monkeypatch):
    # Lista, którą będzie zwracać przysłonięte "zapytanie do API"
    mock_movies_list = ['Movie 1', 'Movie 2']
 
@@ -46,5 +47,23 @@ def test_get_single_movie_cast():
    }
    response = requests.get(endpoint, headers=headers)
    assert response.status_code == 200
-   
 
+def test_homepage(monkeypatch):
+   api_mock = Mock(return_value={'results': []})
+   monkeypatch.setattr("tmdb_client.call_tmdb_api", api_mock)
+
+   with app.test_client() as client:
+       response = client.get('/')
+       assert response.status_code == 200
+       api_mock.assert_called_once_with(f'movie/{list_type}') 
+
+
+@pytest.mark.parametrize('list_type', ['now_playing', 'popular', 'top_rated', 'upcoming'])
+def test_homepage(monkeypatch, list_type):
+   api_mock = Mock(return_value={'results': []})
+   monkeypatch.setattr("tmdb_client.call_tmdb_api", api_mock)
+
+   with app.test_client() as client:
+      response = client.get(f'/?list_type={list_type}')
+      assert response.status_code == 200
+      api_mock.assert_called_once_with(f'movie/{list_type}')
